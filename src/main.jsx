4cec066c3f9782f55409bbdba071772c6cade716
Biswajit Swain <biswajitswain317@gmail.com>
2025-08-16 21:15:04 +0530

    updated2

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import SignInPage from './auth/sign-in/index.jsx'
 import Home from './home/index.jsx'
 import Dashboard from './dashboard/index.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
+import EditResume from './dashboard/resume/[resumeId]/edit/index.jsx'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 const router = createBrowserRouter([
@@ -16,7 +17,12 @@ const router = createBrowserRouter([
       {
         path:'/dashboard',
         element:<Dashboard/>
+      },
+      {
+        path:'/dashboard/resume/:resumeId/edit',
+        element:<EditResume/>
       }
+
     ]
   },
   {
@@ -36,3 +42,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </ClerkProvider>
   </React.StrictMode>,
 )
+
