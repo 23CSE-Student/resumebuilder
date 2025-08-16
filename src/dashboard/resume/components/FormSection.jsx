ec7406426049826d4116655b84c78edf8ec90658
Biswajit Swain <biswajitswain317@gmail.com>
2025-08-17 00:36:06 +0530

    done 30%

--- src/dashboard/resume/components/FormSection.jsx
+++ src/dashboard/resume/components/FormSection.jsx
@@ -4,7 +4,8 @@ import { Button } from '@/components/ui/button'
 import { ArrowLeft, ArrowRight, LayoutGrid } from 'lucide-react'
 
 function FormSection() {
-  const [activeFormIndex,setActiveFormIndex]=useState(2);
+  const [activeFormIndex,setActiveFormIndex]=useState(1);
+  const [enableNext,setEnableNext]=useState(false)
   return (
     <div>
         <div className='flex justify-between items-center'>
@@ -14,14 +15,16 @@ function FormSection() {
             {activeFormIndex>1
             &&<Button size="sm"
             onClick={()=>setActiveFormIndex(activeFormIndex-1)}> <ArrowLeft/> </Button>}
-            <Button className="flex gap-2" size="sm"
+            <Button 
+            disabled={!enableNext}
+            className="flex gap-2" size="sm"
             onClick={()=>setActiveFormIndex(activeFormIndex+1)}
             > Next 
               <ArrowRight/> </Button>
           </div>
         </div>
         {/* Personal Detail */}
-         {activeFormIndex==1? <PersonalDetail/>
+         {activeFormIndex==1? <PersonalDetail enabledNext={(v)=>setEnableNext(v)} />
          :null}
         {/* Summery */}
 
--- src/dashboard/resume/components/forms/PersonalDetail.jsx
+++ src/dashboard/resume/components/forms/PersonalDetail.jsx
@@ -1,18 +1,52 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { ResumeInfoContext } from '@/context/ResumeInfoContext'
-import React, { useContext } from 'react'
+import { LoaderCircle } from 'lucide-react';
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import GlobalApi from './../../../../../service/GlobalApi';
+import { toast } from 'sonner';
 
-function PersonalDetail() {
+function PersonalDetail({enabledNext}) {
 
+    const params=useParams();
     const {resumeInfo,setResumeInfo}=useContext(ResumeInfoContext)
 
-    const handleInputChange=(e)=>{
+    const [formData,setFormData]=useState();
+    const [loading,setLoading]=useState(false);
+    useEffect(()=>{
+        console.log("---",resumeInfo)
+    },[])
 
+    const handleInputChange=(e)=>{
+        enabledNext(false)
+        const {name,value}=e.target;
+
+        setFormData({
+            ...formData,
+            [name]:value
+        })
+        setResumeInfo({
+            ...resumeInfo,
+            [name]:value
+        })
     }
 
     const onSave=(e)=>{
         e.preventDefault();
+        setLoading(true)
+        const data={
+            data:formData
+        }
+        GlobalApi.UpdateResumeDetail(params?.resumeId,data).then(resp=>{
+            console.log(resp);
+            enabledNext(true);
+            setLoading(false);
+            toast("Details updated")
+        },(error)=>{
+            setLoading(false);
+        })
+        
     }
   return (
     <div className='p-5 shadow-lg rounded-lg border-t-primary border-t-4 mt-10'>
@@ -23,34 +57,45 @@ function PersonalDetail() {
             <div className='grid grid-cols-2 mt-5 gap-3'>
                 <div>
                     <label className='text-sm'>First Name</label>
-                    <Input name="firstName" required onChange={handleInputChange} />
+                    <Input name="firstName" defaultValue={resumeInfo?.firstName} required onChange={handleInputChange}  />
                 </div>
                 <div>
                     <label className='text-sm'>Last Name</label>
-                    <Input name="lastName" required onChange={handleInputChange} />
+                    <Input name="lastName" required onChange={handleInputChange} 
+                    defaultValue={resumeInfo?.lastName} />
                 </div>
                 <div className='col-span-2'>
                     <label className='text-sm'>Job Title</label>
-                    <Input name="jobTitle" required onChange={handleInputChange} />
+                    <Input name="jobTitle" required 
+                    defaultValue={resumeInfo?.jobTitle}
+                    onChange={handleInputChange}  />
                 </div>
                 <div className='col-span-2'>
                     <label className='text-sm'>Address</label>
-                    <Input name="address" required onChange={handleInputChange} />
+                    <Input name="address" required 
+                    defaultValue={resumeInfo?.address}
+                    onChange={handleInputChange}  />
                 </div>
                 <div>
                     <label className='text-sm'>Phone</label>
-                    <Input name="phone" required onChange={handleInputChange} />
+                    <Input name="phone" required 
+                    defaultValue={resumeInfo?.phone}
+                    onChange={handleInputChange}  />
                 </div>
                 <div>
                     <label className='text-sm'>Email</label>
-                    <Input name="email" required onChange={handleInputChange} />
+                    <Input name="email" required 
+                    defaultValue={resumeInfo?.email}
+                    onChange={handleInputChange}  />
                 </div>
             </div>
             <div className='mt-3 flex justify-end'>
-                <Button type="submit">Save</Button>
+                <Button type="submit"
+                disabled={loading}>
+                    {loading?<LoaderCircle className='animate-spin' />:'Save'}
+                    </Button>
             </div>
         </form>
-
     </div>
   )
 }
